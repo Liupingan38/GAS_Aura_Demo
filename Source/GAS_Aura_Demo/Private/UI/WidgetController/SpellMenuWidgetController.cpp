99f57666d4780cfd4a86d2e8b10bce3fea731b4a
Liupingan38 <481994145@qq.com>
2025-08-03 19:30:24 +0800

    Spell Menu

---
+++ Source/GAS_Aura_Demo/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright Liupingan
+
+
+#include "UI/WidgetController/SpellMenuWidgetController.h"
+
---
+++ Source/GAS_Aura_Demo/Public/UI/WidgetController/SpellMenuWidgetController.h
@@ -0,0 +1,17 @@
+// Copyright Liupingan
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+#include "SpellMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_AURA_DEMO_API USpellMenuWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+	
+};
